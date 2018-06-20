16e83bd67acb6a62e21c08eeb3ad4821f2c6f5f0
Manu de Frutos Vila <manufvdev@gmail.com>
2018-06-20 16:45:53 +0200

    fix: add karma PhantomJS for travis tests

--- karma.conf.js
+++ karma.conf.js
@@ -57,8 +57,8 @@ module.exports = function(config) {
 
 
     // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+      // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
